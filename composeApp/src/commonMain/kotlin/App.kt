92b9d01225cbd7dae77b70c7f0d46a69ca245703
MohamedMohamed <m01126165016@icloud.com>
2024-07-19 16:35:58 +0300

    main

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -8,8 +8,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import data.MongoDB
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 import presentation.screen.home.HomeScreen
+import presentation.screen.home.HomeViewModel
+import presentation.screen.task.TaskViewModel
 
 val lightRedColor = Color(color = 0xFFF57D88)
 val darkRedColor = Color(color = 0xFF77000B)
@@ -17,17 +22,19 @@ val darkRedColor = Color(color = 0xFF77000B)
 @Composable
 @Preview
 fun App() {
+    initializeKoin()
+
     val lightColors = lightColorScheme(
         primary = lightRedColor,
         onPrimary = darkRedColor,
         primaryContainer = lightRedColor,
-        onPrimaryContainer = darkRedColor,
+        onPrimaryContainer = darkRedColor
     )
     val darkColors = darkColorScheme(
         primary = lightRedColor,
         onPrimary = darkRedColor,
         primaryContainer = lightRedColor,
-        onPrimaryContainer = darkRedColor,
+        onPrimaryContainer = darkRedColor
     )
     val colors by mutableStateOf(
         if (isSystemInDarkTheme()) darkColors else lightColors
@@ -37,5 +44,17 @@ fun App() {
         Navigator(HomeScreen()) {
             SlideTransition(it)
         }
+    }
+}
+
+val mongoModule = module {
+    single { MongoDB() }
+    factory { HomeViewModel(get()) }
+    factory { TaskViewModel(get()) }
+}
+
+fun initializeKoin() {
+    startKoin {
+        modules(mongoModule)
     }
 }
