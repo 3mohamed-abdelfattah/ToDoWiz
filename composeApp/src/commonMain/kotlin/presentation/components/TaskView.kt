2d1b4d724da3de96ae5f3ec4d5ee7afc3fdd37ac
MohamedMohamed <m01126165016@icloud.com>
2024-07-19 16:36:26 +0300

    Task View

--- composeApp/src/commonMain/kotlin/presentation/components/TaskView.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/TaskView.kt
@@ -1,7 +1,11 @@
 package presentation.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
